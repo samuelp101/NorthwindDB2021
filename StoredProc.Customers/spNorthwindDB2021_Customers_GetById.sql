7e963a3005fd6aad96cfb4b51e1625037e4c9294
samuelp101 <samuelphung@live.com>
2021-08-17 23:14:33 -0700

    Updated spNorthwindDB2021_Customers_GetById

--- StoredProc.Customers/spNorthwindDB2021_Customers_GetById.sql
+++ StoredProc.Customers/spNorthwindDB2021_Customers_GetById.sql
@@ -5,6 +5,14 @@
 
 CREATE PROCEDURE dbo.spNorthwindDB2021_Customers_GetById
 	@CustomerId int
+	,@CompanyName nvarchar(40) = null
+	,@ContactName nvarchar(30) = null
+	,@ContactTitle nvarchar(30) = null
+	,@Address nvarchar(60) = null
+	,@City nvarchar(15) = null
+	,@Region nvarchar(15) = null
+	,@PostalCode nvarchar(10) = null
+	,@Country nvarchar(15) = null
 As
 
 -- DECLARE @CustomerId int = 2
