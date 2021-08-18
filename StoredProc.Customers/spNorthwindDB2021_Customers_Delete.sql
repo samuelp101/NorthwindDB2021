6588511270560a24734872688cd8e5dc0d47c24a
samuelp101 <samuelphung@live.com>
2021-08-17 23:18:09 -0700

    Updated spNorthwindDB2021_Customers_Delete

--- StoredProc.Customers/spNorthwindDB2021_Customers_Delete.sql
+++ StoredProc.Customers/spNorthwindDB2021_Customers_Delete.sql
@@ -2,6 +2,14 @@
 
 CREATE PROCEDURE dbo.spNorthwindDB2021_Customers_Delete
 	@CustomerID int
+        ,@CompanyName nvarchar(40) = null
+	,@ContactName nvarchar(30) = null
+	,@ContactTitle nvarchar(30) = null
+	,@Address nvarchar(60) = null
+	,@City nvarchar(15) = null
+	,@Region nvarchar(15) = null
+	,@PostalCode nvarchar(10) = null
+	,@Country nvarchar(15) = null
 As
 
 -- DECLARE @CustomerID int = 92 
